fef0d7a9038cc77452b136959397f7b7411ac161
Muhammad Anas Jamshaid <145829950+muhammadanasjamshaid@users.noreply.github.com>
2024-03-26 16:43:19 +0500

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstnumber" },
+    { message: "Enter Second Number", type: "number", name: "secondnumber" },
+    {
+        message: "Select one of the opreators to perform action",
+        type: "list",
+        name: "opreator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// conditional statement
+if (answer.opreator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.opreator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.opreator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.opreator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("Please Select Valid Opreators");
+}
